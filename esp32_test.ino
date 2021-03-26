823bf87864ade86da53829d5f3cb04aa15234a67
zhaowenhao <zwh877719756@163.com>
2021-03-26 11:41:48 +0800

    增加定时中断，定时访问天气API

--- esp32_test.ino
+++ esp32_test.ino
@@ -14,24 +14,41 @@ const char *host = "https://api.seniverse.com"; //心知天气APIhost
 const char *apiKey = "S5Z6TKOCrnZtNe7gr";       //API key 私钥
 const char *city = "shanghai";                     //查询的城市
 
+int http_code = 0;
+int time_flag = 0;
+
 String req;
 String rsp;
 String city_name;
 String city_weather;
 String city_temperature;
 
+//HTTPClint对象
 HTTPClient http_client;
-//
+//JSON对象
 StaticJsonDocument<512> doc;
-
+//oled对象
 SSD1306 oled(0x3c, 26, 25);
+// 定时器指针
+hw_timer_t * timer = NULL;
+// timer interruption function
+void IRAM_ATTR onTimer(){
+  // Increment the counter and set the time of ISR
+  if (time_flag <= 4)
+  {
+    /* code */
+    time_flag++;
+  } else
+  time_flag = 0;
+  
+}
 
 void setup()
 {
   //OLED
   oled.init();
   oled.setFont(ArialMT_Plain_16);
-  // oled.drawString(0, 0, "shanghai!");
+  oled.drawString(0, 16, "your city weather!");
   oled.display();
   oled.clear();
   
@@ -51,7 +68,7 @@ void setup()
   Serial.println("OK");
   Serial.println("Wifi connected");
   
-  //HTTPClint https://api.seniverse.com/v3/weather/now.json?key=S5Z6TKOCrnZtNe7gr&location=shanghai&language=en&unit=c
+  //待访问的API https://api.seniverse.com/v3/weather/now.json?key=S5Z6TKOCrnZtNe7gr&location=shanghai&language=en&unit=c
   req = (String)host + "/v3/weather/now.json?key=";
   req += apiKey;
   req += "&location=";
@@ -62,53 +79,59 @@ void setup()
   {
     Serial.println("HTTPclient setUp done!");
   }
+  // 定时中断
+  // Use 1st timer of 4 (counted from zero).
+  // Set 80 divider for prescaler (see ESP32 Technical Reference Manual for more
+  // info).
+  timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer, &onTimer, true);
+  // // Set alarm to call onTimer function every second (value in microseconds).
+  // // Repeat the alarm (third parameter)
+  timerAlarmWrite(timer, 1000000, true); // 10 second T
+  // // Start an alarm
+  timerAlarmEnable(timer);
 }
 
 void loop()
 {
-  int http_code = http_client.GET();
-  //Serial.println(http_code);
-  if (http_code > 0)
+  if (time_flag == 5)
   {
-    //Serial.printf("HTTP get code: %d\n", http_code);
-    if (http_code == HTTP_CODE_OK)
+    http_code = http_client.GET();
+    if (http_code > 0)
     {
-      rsp = http_client.getString();
-      
-      DeserializationError error = deserializeJson(doc, rsp);
-      if (error) {
-        Serial.print(F("deserializeJson() failed: "));
-        Serial.println(error.f_str());
-        return;
+      Serial.printf("HTTP get code: %d\n", http_code);
+      if (http_code == HTTP_CODE_OK)
+      {
+        rsp = http_client.getString();
+        // 解析JSON
+        DeserializationError error = deserializeJson(doc, rsp);
+        if (error) {
+          Serial.print(F("deserializeJson() failed: "));
+          Serial.println(error.f_str());
+          return;
+        }
+        JsonObject results_0 = doc["results"][0];
+        JsonObject results_0_location = results_0["location"];
+        const char* results_0_location_name = results_0_location["name"]; // "上海"
+    
+        JsonObject results_0_now = results_0["now"];
+        const char* results_0_now_text = results_0_now["text"];
+        const char* results_0_now_temperature = results_0_now["temperature"];
+        // 打印天气
+        oled.drawString(0, 0, results_0_location_name);
+        oled.drawString(0, 16, results_0_now_text);
+        oled.drawString(65, 16, results_0_now_temperature);
+        oled.drawString(85, 16, "°C");
+        oled.display();
+
+        // Serial.println(results_0_location_name);
+        // Serial.println(results_0_now_text);
+        // Serial.println(results_0_now_temperature);
       }
-      JsonObject results_0 = doc["results"][0];
-      JsonObject results_0_location = results_0["location"];
-//      const char* results_0_location_id = results_0_location["id"]; // "WTW3SJ5ZBJUY"
-      const char* results_0_location_name = results_0_location["name"]; // "上海"
-//      const char* results_0_location_country = results_0_location["country"]; // "CN"
-//      const char* results_0_location_path = results_0_location["path"]; // "上海,上海,中国"
-//      const char* results_0_location_timezone = results_0_location["timezone"]; // "Asia/Shanghai"
-//      const char* results_0_location_timezone_offset = results_0_location["timezone_offset"]; // "+08:00"
-//      
-      JsonObject results_0_now = results_0["now"];
-      const char* results_0_now_text = results_0_now["text"];
-//      const char* results_0_now_code = results_0_now["code"]; // "9"
-      const char* results_0_now_temperature = results_0_now["temperature"];
-
-      oled.drawString(0, 0, results_0_location_name);
-      oled.drawString(0, 16, results_0_now_text);
-      oled.drawString(50, 16, results_0_now_temperature);
-      oled.drawString(70, 16, "°C");
-      oled.display();
-
-      // Serial.println(results_0_location_name);
-      // Serial.println(results_0_now_text);
-      // Serial.println(results_0_now_temperature);
-    }
-    else
-    {
-      Serial.printf("fail to get cityWeather,code:%d\n", http_code);
+      else
+      {
+        Serial.printf("fail to get cityWeather,code:%d\n", http_code);
+      }
     }
   }
-  delay(5000);
 }
